5adb91ceb1abec1619a4b2e53573e2a1792e6854
Thomas Körner <t.koerner@heimrich-hannot.de>
2024-03-14 12:07:01 +0100

    remove asset integration

--- src/EventListener/Contao/InitializeSystemListener.php
+++ src/EventListener/Contao/InitializeSystemListener.php
@@ -20,23 +20,14 @@ use Symfony\Component\HttpFoundation\RequestStack;
 class InitializeSystemListener
 {
     protected BookingPlanAction $bookingPlanAction;
-    private RequestStack        $requestStack;
-    private ScopeMatcher        $scopeMatcher;
 
-    public function __construct(BookingPlanAction $bookingPlanAction, RequestStack $requestStack, ScopeMatcher $scopeMatcher)
+    public function __construct(BookingPlanAction $bookingPlanAction)
     {
         $this->bookingPlanAction = $bookingPlanAction;
-        $this->requestStack = $requestStack;
-        $this->scopeMatcher = $scopeMatcher;
     }
 
     public function __invoke(): void
     {
         Registry::add($this->bookingPlanAction);
-
-        if (($request = $this->requestStack->getCurrentRequest()) && $this->scopeMatcher->isBackendRequest($request)) {
-            $GLOBALS['TL_CSS']['huh_isotope_resource_booking'] = 'bundles/heimrichhannotisotoperesourcebooking/backend/css/huh_isotope_resource_booking_backend.css|static';
-            $GLOBALS['TL_JAVASCRIPT']['huh_isotope_resource_booking'] = 'bundles/heimrichhannotisotoperesourcebooking/backend/js/huh_isotope_resource_booking_backend.js|static';
-        }
     }
 }
